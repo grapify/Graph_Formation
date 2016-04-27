1299ef5b6e71f41b3a0fc6a08ea72d1c8037f11d
sgmonusg <sagargupta_monu@yahoo.co.in>
2016-04-28 01:40:28 +0530

    mian()_modified

--- basic_graph.c
+++ basic_graph.c
@@ -16,38 +16,69 @@ int numberOfNodes(){
     return vec.size();
 }
 
-boolean isConnect(node *node1,node *node2){
+boolean isConnect(){
         if()
 }
 
 
 
 
-void AddEdge(int data,int data1){
-    node=findNode(int data);
-    node=findNode(int data1);
+void AddEdge(int data,int data1,int weight){
+    vec[data].push_back(make_pair(data1, weight));
 
 
+}
+
+void addNode(int data){
+    ve[data].push_back(make_pair(null,null));
+}
+
+void menu(){
 
 }
 
 int main(){
     menu();
     int a=0;
-    while(n<5){
+    while(a<5){
         cin>>x;
         if(x==0){
             int data;
             cin>>data;
-            head=createNode(data);
-            AddNode(head)
+            addNode(int data);
         }
         if(x==1){
             int data;
             int data1;
-            cin>>data>>data1;
-            AddEdge(data,data1);
+            int weight;
+            cin>>data>>data1>>weight;
+            AddEdge(data,data1,weight);
             i++;
+        }
+        else if(x==2){
+            printf("%d\n",numberOfNodes);
+        }
+        else if(x==3){
+            printf("%d\n",numberOfEdges);
+        }
+        else if(x==4){
+            int data1;
+            int data;
+            cin>>data>>data1;
+            isConnect(int data,int data1);
+        }
+        else if(x==5){
+            int des;
+            int start;
+            cin>>des>>start;
+            shortestPath(des,start);
+        }
+        else if(x==6){
+            int des;
+            int start;
+            cin>>des>>start;
+            allPAth(des,start);
+
         }
     }
     return 0;
