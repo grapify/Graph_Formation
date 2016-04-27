cf6e1c052e57f2cdc7dc37d8fe10607a3d715a0c
sgmonusg <sagargupta_monu@yahoo.co.in>
2016-04-27 22:49:06 +0530

    basic_addition_of_func

---
+++ basic_graph.c
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std
+
+struct node{
+    int data;
+    int in;
+    int out;
+};
+
+int numberOfEdges(){
+
+}
+
+int numberOfNodes(){
+
+}
+
+boolean isConnect(node *node1,node *node2){
+
+}
+
+void AddNode(node *node){
+
+}
+
+void AddEdge(node *node1,node *node2){
+
+}
+int main(){
+
+}
