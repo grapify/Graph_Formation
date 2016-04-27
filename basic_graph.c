e5ffc30eb276f9c8cf750f50bae47da1d0bef9b1
sgmonusg <sagargupta_monu@yahoo.co.in>
2016-04-28 01:28:28 +0530

    changed_perspective()

--- basic_graph.c
+++ basic_graph.c
@@ -1,33 +1,54 @@
 #include <iostream>
-
+#include <vector>
 using namespace std
+//create a vector
 
-struct node{
-    int data;
-    int in;
-    int out;
-};
+vector<list<push<int ,int>> vec;
+int i;
+
+
 
 int numberOfEdges(){
-
+        return i;
 }
 
 int numberOfNodes(){
-
+    return vec.size();
 }
 
 boolean isConnect(node *node1,node *node2){
-
+        if()
 }
 
-void AddNode(node *node){
 
-}
 
-void AddEdge(node *node1,node *node2){
+
+void AddEdge(int data,int data1){
+    node=findNode(int data);
+    node=findNode(int data1);
+
+
 
 }
 
 int main(){
-
+    menu();
+    int a=0;
+    while(n<5){
+        cin>>x;
+        if(x==0){
+            int data;
+            cin>>data;
+            head=createNode(data);
+            AddNode(head)
+        }
+        if(x==1){
+            int data;
+            int data1;
+            cin>>data>>data1;
+            AddEdge(data,data1);
+            i++;
+        }
+    }
+    return 0;
 }
